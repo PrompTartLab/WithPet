8a097cf983e3bc2cf3664df7062b3a24d30a3cfb
milhaud <milhaud1201@gmail.com>
2025-02-16 00:44:17 +0900

    style: apply code formatting

--- nodes/generate_final_answer.py
+++ nodes/generate_final_answer.py
@@ -36,6 +36,4 @@ class HandleNotRelevantNode(BaseNode):
 
 class HandleNoDataNode(BaseNode):
     def execute(self, state):
-        return GraphState(
-            answer="해당 질문에 해당하는 장소를 찾지 못했습니다. 새로운 조건으로 질문해주세요."
-        )
+        return GraphState(answer="해당 질문에 해당하는 장소를 찾지 못했습니다. 새로운 조건으로 질문해주세요.")
